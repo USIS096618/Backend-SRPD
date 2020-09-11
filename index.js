9b7f28ae1a5fd58c3e1982ccf147e02e2fa17a3c
stanley-crtz <johansson.05@hotmail.com>
2020-09-11 14:19:24 -0600

    Servicio de conexion con la base de datos

--- index.js
+++ index.js
@@ -8,10 +8,13 @@ mongoose.set('useFindAndModify', false);
 
 mongoose.Promise = global.Promise;
 
-// mongoose.connect(config.db, { useNewUrlParser: true , useUnifiedTopology: true})
-//         .then(() => {
+mongoose.connect(config.db, { useNewUrlParser: true , useUnifiedTopology: true, useFindAndModify: true, useCreateIndex: true})
+        .then(() => {
             app.listen(config.port, () => {
                 console.log('Servidor corriendo en http://localhost:'+config.port);
             });
 
-        // });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
