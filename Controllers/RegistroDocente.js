662cb7e97b0cf81541f209eb760f06f17f8362fb
stanley-crtz <johansson.05@hotmail.com>
2020-09-17 22:00:15 -0600

    Arreglo de la extencion de la imagen

--- Controllers/RegistroDocente.js
+++ Controllers/RegistroDocente.js
@@ -174,7 +174,7 @@ const RegistroDocente = {
         fileName = req.file.filename;
 
         var fileExtension = req.file.originalname.split('\.');
-        var extension = fileExtension[0];
+        var extension = fileExtension[1];
 
        if (extension != 'png' && extension != 'jpeg' && extension != 'gif' && extension != 'jpg'){
             fs.unlink(filePath, (err) => {
