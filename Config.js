60a7b4f6f58cbfd52096a35b380001efd31f08f5
USIS096618 <60713895+USIS096618@users.noreply.github.com>
2021-08-27 21:20:15 -0600

    un comentario

--- Config.js
+++ Config.js
@@ -1,6 +1,6 @@
 module.exports = {
     port: process.env.PORT || 3900,
     // db: process.env.MONGODB || 'mongodb://localhost:27017/SRP',
-    db:'mongodb+srv://codeMaster:codeMaster@cluster0.xq7jk.mongodb.net/test',
+    db:'mongodb+srv://nov132018:nov132018@cluster0.wunvs.mongodb.net/myFirstDatabase',
     SECRET_TOKEN: 'miclavedetokens'
 }
